b4a3f93964bf8a50fa6c9a23b9f9922049c838db
pierre travacca <pierre.travacca@gmail.com>
2021-03-15 14:19:26 +0100

    fix : web routes order

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
-/** HOME PAGE */
-Route::get('/{access_id?}', 'AppController@welcome')
-	->name('welcome');
+/** ADMIN VIEW */
+Route::get('/admin', 'AppController@admin')->middleware('auth.basic');
+
 /** RECORD DREAM PAGE */
 Route::get('/record_dream', 'AppController@record_dream')
 	->name('record_dream');
-/** ADMIN VIEW */
-Route::get('/admin', 'AppController@admin')->middleware('auth.basic');
+
+/** HOME PAGE */
+Route::get('/{access_id?}', 'AppController@welcome')
+	->name('welcome');
